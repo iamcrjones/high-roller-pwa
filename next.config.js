a5de5f375405f3c9178580946bb9211879d67c08
Cameron <cameron.jones@monarcglobal.com>
2023-09-08 12:42:38 +1000

    config setup init

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
