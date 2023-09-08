c5177b8ee54852d765b423d68f73942a56868779
Cameron <cameron.jones@monarcglobal.com>
2023-09-08 13:51:56 +1000

    prettier conf, testing tcss

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
