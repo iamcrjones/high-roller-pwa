3cdfcb932990b03ae4451a990113aaac96b7bf4c
Cameron <cameron.jones@monarcglobal.com>
2023-09-09 21:42:52 +1000

    dice select init

--- src/pages/roll-page/index.tsx
+++ src/pages/roll-page/index.tsx
@@ -4,18 +4,19 @@ import React, { useState } from "react";
 const RollPage = () => {
   const [result, setResult] = useState<number | number[]>();
   const [diceAmount, setDiceAmount] = useState(1);
+  const [diceType, setDiceType] = useState("d6");
+
   const handleRoll = (amount: number) => {
-    console.log({ amount });
+    const rollNum = diceType.split("d")[1];
     if (amount === 1) {
-      const roll = rollDice(1, 6);
+      const roll = rollDice(1, +rollNum!);
       setResult(roll);
     } else {
       const results: number[] = [];
       for (let i = 0; i < amount; i++) {
-        const roll = rollDice(1, 6);
+        const roll = rollDice(1, +rollNum!);
         results.push(roll);
       }
-      console.log({ results });
       setResult(results);
     }
   };
@@ -37,6 +38,17 @@ const RollPage = () => {
         <option value={4}>4</option>
         <option value={5}>5</option>
         <option value={6}>6</option>
+      </select>
+      <select
+        defaultValue={diceType}
+        onChange={(e) => setDiceType(e.target.value)}
+        className="bg-black text-red-400"
+      >
+        <option value={"d6"}>d6</option>
+        <option value={"d8"}>d8</option>
+        <option value={"d10"}>d10</option>
+        <option value={"d12"}>d12</option>
+        <option value={"d20"}>d20</option>
       </select>
       {result && typeof result === "object" && result.length > 1 ? (
         <h1>
