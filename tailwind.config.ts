50fc6ea7023b29085e273b98496ef59c828e5975
Cameron <cameron.jones@monarcglobal.com>
2023-09-12 07:45:57 +1000

    font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { fontFamily: { roboto: ["Roboto Mono", "monospace"] } },
   },
   plugins: [],
 } satisfies Config;
