8f38f0ba2baa091df9616e72c200deef6caaa4fb
Cameron <cameron.jones@monarcglobal.com>
2023-09-11 20:45:28 +1000

    modal display for single or arr results

--- src/pages/roll-page/components/results-modal.tsx
+++ src/pages/roll-page/components/results-modal.tsx
@@ -8,9 +8,19 @@ type ModalProps = {
 function ResultsModal({ results, close }: ModalProps) {
   console.log({ results });
   return (
-    <div className=" absolute inset-0 z-40 flex flex-col items-center bg-black opacity-75">
+    <div className=" absolute inset-0 z-40 flex flex-col items-center bg-black text-white opacity-75">
       <div className="flex h-full flex-col justify-between">
-        <h1 className="py-8 text-4xl text-white">You Rolled</h1>
+        <h1 className="py-8 text-4xl">You Rolled</h1>
+        <div>
+          {typeof results === "number" ? (
+            <h1>single result {results}</h1>
+          ) : (
+            <>
+              <h1>array result</h1>{" "}
+              {results?.map((r, ix) => <h1 key={ix}>{r}</h1>)}
+            </>
+          )}
+        </div>
         <button onClick={close} className="mb-12 bg-red-700 text-white">
           close
         </button>
