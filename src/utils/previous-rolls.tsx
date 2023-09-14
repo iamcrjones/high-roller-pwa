a9f6792f6962a32a1dffa356118f45bc4eb0bf3f
Cameron <cameron.jones@monarcglobal.com>
2023-09-14 13:35:47 +1000

    textchange. center prev

--- src/utils/previous-rolls.tsx
+++ src/utils/previous-rolls.tsx
@@ -9,32 +9,34 @@ const PreviousRolls = ({ prevResults, removedRoll }: PrevRollsProps) => {
   console.log("in prev");
   return (
     <div>
-      <p className="text-2xl">Rolls</p>
-      {prevResults.map((result, ix) => {
-        return (
-          <div
-            key={`${result.roll}${result.modifier}${ix}${Date.now()}`}
-            className={`${
-              ix === 0 ? "animate-slide-down-0" : "animate-slide-down-1"
-            }`}
-          >
-            {result.modifier && result.modifier !== 0 ? (
-              <div className="animate-slide-down flex space-x-4">
-                <p className="text-2xl">{result.roll + result.modifier}</p>
-                <p className="text-2xl text-gray-400">
-                  {result.roll} {result.modifier < 0 ? "-" : "+"}{" "}
-                  {Math.abs(result.modifier)}
-                </p>
-              </div>
-            ) : (
-              <p className="text-2xl">{result.roll}</p>
-            )}
-          </div>
-        );
-      })}
+      <p className="text-center text-2xl">Previous Rolls</p>
+      <div className="flex flex-col items-center justify-center">
+        {prevResults.map((result, ix) => {
+          return (
+            <div
+              key={`${result.roll}${result.modifier}${ix}${Date.now()}`}
+              className={`${
+                ix === 0 ? "animate-slide-down-0" : "animate-slide-down-1"
+              }`}
+            >
+              {result.modifier && result.modifier !== 0 ? (
+                <div className="animate-slide-down flex space-x-4">
+                  <p className="text-2xl">{result.roll + result.modifier}</p>
+                  <p className="text-2xl text-gray-400">
+                    {result.roll} {result.modifier < 0 ? "-" : "+"}{" "}
+                    {Math.abs(result.modifier)}
+                  </p>
+                </div>
+              ) : (
+                <p className="text-2xl">{result.roll}</p>
+              )}
+            </div>
+          );
+        })}
+      </div>
       {removedRoll ? (
         <div
-          className="animate-slide-out"
+          className="flex animate-slide-out items-center justify-center"
           key={`${removedRoll.roll}${Date.now()}`}
         >
           {removedRoll.modifier && removedRoll.modifier !== 0 ? (
